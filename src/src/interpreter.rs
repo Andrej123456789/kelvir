62f14022430a6932261baf93c7588e5c9a8459ea
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2023-09-15 19:58:38 +0200

    Fixed additon, subtraction & multiplication for single digit numbers

--- src/src/interpreter.rs
+++ src/src/interpreter.rs
@@ -1,35 +1,33 @@
 #![allow(non_snake_case)]
 
 use super::nodes::*;
-use super::values::{Number};
+use super::values::Number;
 
-pub struct Interpreter {
+pub struct Interpreter {}
 
-}
-
-macro_rules! perform_operation {
-    ($inner:expr, $operator:expr) => {{
-        let mut value_a = 1.0;
-        let mut value_b = 1.0;
-
-        match $inner.node_a.clone() {
-            ALL_VARIANT::Number(rc) => {
-                let number = &*rc;
-                value_a = number.value;
-            }
-            _ => unreachable!(),
+fn extract_value(node: ALL_VARIANT) -> Option<f64> {
+    match node {
+        ALL_VARIANT::Number(rc) => Some(rc.value),
+        ALL_VARIANT::Add(inner) => {
+            extract_value(inner.node_a.clone());
+            extract_value(inner.node_b.clone())
         }
-
-        match $inner.node_b.clone() {
-            ALL_VARIANT::Number(rc) => {
-                let number = &*rc;
-                value_b = number.value;
-            }
-            _ => unreachable!(),
+        ALL_VARIANT::Sub(inner) => {
+            extract_value(inner.node_a.clone());
+            extract_value(inner.node_b.clone())
         }
-
-        Number { value: $operator(value_a, value_b) }
-    }};
+        ALL_VARIANT::Mul(inner) => {
+            extract_value(inner.node_a.clone());
+            extract_value(inner.node_b.clone())
+        }
+        ALL_VARIANT::Div(inner) => {
+            extract_value(inner.node_a.clone());
+            extract_value(inner.node_b.clone())
+        }
+        ALL_VARIANT::Plus(inner) => extract_value(inner.node.clone()),
+        ALL_VARIANT::Minus(inner) => extract_value(inner.node.clone()),
+        _ => None,
+    }
 }
 
 impl Interpreter {
@@ -40,54 +38,74 @@ impl Interpreter {
             ALL_VARIANT::Sub(_) => self.visit_SubtractNode(node),
             ALL_VARIANT::Mul(_) => self.visit_MultiplyNode(node),
             ALL_VARIANT::Div(_) => self.visit_DivideNode(node),
-            _ => return Number { value: (0.0) }
+            _ => return Number { value: (0.0) },
         }
     }
 
     fn visit_NumberNode(&self, node: ALL_VARIANT) -> Number {
         match node {
-            ALL_VARIANT::Number(inner) => return Number {value: (inner.value) },
+            ALL_VARIANT::Number(inner) => {
+                return Number {
+                    value: (inner.value),
+                }
+            }
             _ => unreachable!(),
         };
     }
 
     fn visit_AddNode(&self, node: ALL_VARIANT) -> Number {
         match node {
-            ALL_VARIANT::Add(inner) => return perform_operation!(inner, |a, b| a + b),
-            ALL_VARIANT::Sub(inner) => return perform_operation!(inner, |a, b| a + b),
-            ALL_VARIANT::Mul(inner) => return perform_operation!(inner, |a, b| a + b),
-            ALL_VARIANT::Div(inner) => return perform_operation!(inner, |a, b| a + b),
+            ALL_VARIANT::Add(_rc) => {
+                let rc = &*_rc;
+                return Number {
+                    value: extract_value(rc.node_a.clone()).unwrap_or(0.0)
+                        + extract_value(rc.node_b.clone()).unwrap_or(0.0),
+                };
+            }
+
             _ => unreachable!(),
-        };
+        }
     }
 
     fn visit_SubtractNode(&self, node: ALL_VARIANT) -> Number {
         match node {
-            ALL_VARIANT::Add(inner) => return perform_operation!(inner, |a, b| a - b),
-            ALL_VARIANT::Sub(inner) => return perform_operation!(inner, |a, b| a - b),
-            ALL_VARIANT::Mul(inner) => return perform_operation!(inner, |a, b| a - b),
-            ALL_VARIANT::Div(inner) => return perform_operation!(inner, |a, b| a - b),
+            ALL_VARIANT::Sub(_rc) => {
+                let rc = &*_rc;
+                return Number {
+                    value: extract_value(rc.node_a.clone()).unwrap_or(0.0)
+                        - extract_value(rc.node_b.clone()).unwrap_or(0.0),
+                };
+            }
+
             _ => unreachable!(),
-        };
+        }
     }
 
     fn visit_MultiplyNode(&self, node: ALL_VARIANT) -> Number {
         match node {
-            ALL_VARIANT::Add(inner) => return perform_operation!(inner, |a, b| a * b),
-            ALL_VARIANT::Sub(inner) => return perform_operation!(inner, |a, b| a * b),
-            ALL_VARIANT::Mul(inner) => return perform_operation!(inner, |a, b| a * b),
-            ALL_VARIANT::Div(inner) => return perform_operation!(inner, |a, b| a * b),
+            ALL_VARIANT::Mul(_rc) => {
+                let rc = &*_rc;
+                return Number {
+                    value: extract_value(rc.node_a.clone()).unwrap_or(0.0)
+                        * extract_value(rc.node_b.clone()).unwrap_or(0.0),
+                };
+            }
+
             _ => unreachable!(),
-        };
+        }
     }
 
     fn visit_DivideNode(&self, node: ALL_VARIANT) -> Number {
         match node {
-            ALL_VARIANT::Add(inner) => return perform_operation!(inner, |a, b| a / b),
-            ALL_VARIANT::Sub(inner) => return perform_operation!(inner, |a, b| a / b),
-            ALL_VARIANT::Mul(inner) => return perform_operation!(inner, |a, b| a / b),
-            ALL_VARIANT::Div(inner) => return perform_operation!(inner, |a, b| a / b),
+            ALL_VARIANT::Div(_rc) => {
+                let rc = &*_rc;
+                return Number {
+                    value: extract_value(rc.node_a.clone()).unwrap_or(0.0)
+                        / extract_value(rc.node_b.clone()).unwrap_or(1.0),
+                };
+            }
+
             _ => unreachable!(),
-        };
+        }
     }
 }
